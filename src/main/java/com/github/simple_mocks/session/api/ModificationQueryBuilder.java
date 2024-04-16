0e487e8d00d18e411702b865a2090d6551c2e06b
sibmaks <sibmaks@vk.com>
2024-04-17 01:46:50 +0300

    Extend ModificationQueryBuilder

--- src/main/java/com/github/simple_mocks/session/api/ModificationQueryBuilder.java
+++ src/main/java/com/github/simple_mocks/session/api/ModificationQueryBuilder.java
@@ -1,6 +1,7 @@
 package com.github.simple_mocks.session.api;
 
 import com.github.simple_mocks.session.api.dto.action.Action;
+import com.github.simple_mocks.session.api.dto.action.AddAction;
 import com.github.simple_mocks.session.api.dto.action.DeleteAction;
 import com.github.simple_mocks.session.api.dto.action.SetAction;
 import com.github.simple_mocks.session.api.impl.ModificationQueryImpl;
@@ -18,20 +19,17 @@ import java.util.List;
 public final class ModificationQueryBuilder {
     private final List<Action> actions;
 
-    private ModificationQueryBuilder(Action action) {
+    private ModificationQueryBuilder() {
         this.actions = new ArrayList<>();
-        this.actions.add(action);
     }
 
     /**
      * Create builder instance of {@link ModificationQueryBuilder}
      *
-     * @param action 1st action
-     * @param <T>    type of action
      * @return instance of {@link ModificationQueryBuilder}
      */
-    public static <T extends Action> ModificationQueryBuilder builder(T action) {
-        return new ModificationQueryBuilder(action);
+    public static ModificationQueryBuilder builder() {
+        return new ModificationQueryBuilder();
     }
 
     /**
@@ -44,7 +42,7 @@ public final class ModificationQueryBuilder {
     }
 
     /**
-     * Add put attribute value action<br/>
+     * Add create attribute action<br/>
      *
      * @param section   section in session
      * @param attribute attribute code
@@ -52,9 +50,26 @@ public final class ModificationQueryBuilder {
      * @param <T>       type of attribute
      * @return self reference
      */
-    public <T extends Serializable> ModificationQueryBuilder putAttribute(@Nonnull String section,
-                                                                          @Nonnull String attribute,
-                                                                          T value) {
+    public <T extends Serializable> ModificationQueryBuilder create(@Nonnull String section,
+                                                                    @Nonnull String attribute,
+                                                                    T value) {
+        var action = new AddAction(section, attribute, value);
+        this.actions.add(action);
+        return this;
+    }
+
+    /**
+     * Add change attribute value action<br/>
+     *
+     * @param section   section in session
+     * @param attribute attribute code
+     * @param value     attribute value
+     * @param <T>       type of attribute
+     * @return self reference
+     */
+    public <T extends Serializable> ModificationQueryBuilder change(@Nonnull String section,
+                                                                    @Nonnull String attribute,
+                                                                    T value) {
         var action = new SetAction(section, attribute, value);
         this.actions.add(action);
         return this;
@@ -67,8 +82,8 @@ public final class ModificationQueryBuilder {
      * @param attribute attribute code
      * @return self reference
      */
-    public ModificationQueryBuilder removeAttribute(@Nonnull String section,
-                                                    @Nonnull String attribute) {
+    public ModificationQueryBuilder remove(@Nonnull String section,
+                                           @Nonnull String attribute) {
         var action = new DeleteAction(section, attribute);
         this.actions.add(action);
         return this;
